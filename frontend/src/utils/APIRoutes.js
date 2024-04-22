69b85429b24d17167926939f48585ece0c58de3e
Shakeel Ahmed <iamshakeelahmed5@gmail.com>
2024-04-22 13:17:22 +0500

    finalize and testing

---
+++ frontend/src/utils/APIRoutes.js
@@ -0,0 +1,7 @@
+export const host = "http://localhost:3000";
+export const signInRoute = `${host}/api/auth/login`;
+export const signUpRoute = `${host}/api/auth/register`;
+export const signOutRoute = `${host}/api/auth/logout`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
